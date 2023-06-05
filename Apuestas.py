42e6bc4b43a197a88b8df5f336f9ac7e5b861364
SantinoZucconi <santinozucconi@gmail.com>
2023-06-05 11:12:44 -0300

    Agregando ingreso de usuario

--- Apuestas.py
+++ Apuestas.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import csv
+from passlib.hash import pbkdf2_sha256 as plib
 
 def respuesta_api(url , header, endpoint, *nombre) -> dict:
     respuesta = requests.request("GET",url = (url + endpoint), headers = header)
@@ -19,19 +20,69 @@ def leer_archivo_respuesta_api(archivo_respuesta) -> dict:
     datos = json.loads(datos)
     return datos
 
-def main():
-    url = "https://v3.football.api-sports.io/"
+def pedir_usuario() -> dict:
+    mail = input("Ingrese su mail: ")
+    nombre = input("Ingrese su nombre de usuario: ")
+    contraseña = input("Ingrese su contraseña: ")
 
+    usuario = ingreso_de_usuario(mail,nombre,contraseña, archivo_usuarios = "usuarios.csv")
+
+    return usuario
+
+def ingreso_de_usuario(mail, nombre_de_usuario, contraseña, archivo_usuarios) -> dict:
+
+    usuarios = []
+    usuario_coincidente = "no encontrado"
+
+    with open(archivo_usuarios, "r", newline = "", encoding = "utf-8-sig") as archivo:
+        reader = csv.reader(archivo, delimiter = ",")
+        next(reader)
+        for row in reader:
+            if row != []:
+                usuarios.append({"id": row[0], "nombre": row[1], "contraseña": row[2], "cantidad apostada": row[3], "fecha ultima apuesta": row[4], "dinero disponible": row[5]})
+    
+    for i in range(len(usuarios)):
+        if (usuarios[i]['id'] == mail):
+            if(verificar_contraseña(contraseña, usuarios[i]['contraseña']) == True and usuarios[i]['nombre'] == nombre_de_usuario):
+                usuario_coincidente = usuarios[i]
+                break
+            else:
+                print("El usuario o la contraseña es incorrecta")
+                usuario_coincidente = "usuario o contraseña incorrecta"
+
+    if (usuario_coincidente == "usuario o contraseña incorrecta"):
+        pedir_usuario()
+                           
+    if (usuario_coincidente == "no encontrado"):
+        contraseña = plib.hash(contraseña)
+        with open(archivo_usuarios, "a", newline = "", encoding = "utf-8-sig") as archivo:
+            writer = csv.writer(archivo, delimiter=",", quotechar = '"', quoting = csv.QUOTE_NONNUMERIC)
+            writer.writerow((mail,nombre_de_usuario,contraseña,0,"",0))  
+        usuario_coincidente = {"id": mail, "nombre": nombre_de_usuario, "contraseña": contraseña, "cantidad apostada": 0, "fecha ultima apuesta": "", "dinero disponible": 0}
+
+    return usuario_coincidente
+
+def verificar_contraseña(contraseña, hash) -> bool:
+    if (plib.verify(contraseña, hash) == True):
+        return True
+    else:
+        return False
+
+
+def main():
+
+    url = "https://v3.football.api-sports.io/"
     header = {"x-rapidapi-host": "v3.football.api-sports.io",
               "x-rapidapi-key": "f0c007a556a4cabc316ce1a8afa0d95a"}
     endpoint = "standings?league=128&season=2023"
     
-    respuesta = leer_archivo_respuesta_api(archivo_respuesta = "respuesta_liga_argentina.txt")
+    respuesta = leer_archivo_respuesta_api(archivo_respuesta = "respuesta_liga_argentina.txt") #esta funcion se usa una vez hecho la request a la api, asi evitamos gastar los usos que nos da la pagina
+    
     liga = respuesta["response"][0]['league']['standings'][1] #lista, cada indice es un equipo
 
+    usuario = pedir_usuario()
     
-    for i in range(len(liga)):
-        print(f"{i+1}.{liga[i]['team']['name']}") #imprime la tabla de posiciones
+    #print(liga[0])
 
                 
 
